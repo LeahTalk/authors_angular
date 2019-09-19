bc9b8f74638b58f3d5892fcc5385dbb2693fceb2
Leemat44 <ltalkov@zagmail.gonzaga.edu>
2019-09-19 15:19:22 -0700

    added quotes to authors with CRUD actions

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,9 @@
 const controller = require('../controllers/controller.js');
 
+const path = require("path");
+
 module.exports = function(app) {
+
     app.get('/authors', (req, res) => {
         controller.index(req, res);
     });
@@ -19,5 +22,21 @@ module.exports = function(app) {
 
     app.put('/authors/:id/', (req, res) => {
         controller.update_author(req, res);
+    });
+
+    app.put('/quotes/:id', (req, res) => {
+        controller.add_quote(req, res);
+    });
+
+    app.put('/delete_quote/:id', (req, res) => {
+        controller.delete_quote(req, res);
+    });
+
+    app.put('/update_quote/:id', (req, res) => {
+        controller.update_quote(req, res);
+    }); 
+
+    app.all("*", (req,res,next) => {
+        res.sendFile(path.resolve("./public/dist/public/index.html"))
     });
 }
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -8,7 +8,9 @@ module.exports = {
     },
 
     add_author: function(req, res) {
-        Author.create(req.body,{runValidators: true})
+        const author = new Author();
+        author.name = req.body.name;
+        author.save()
             .then(data => 
                 res.json(data)
             )
@@ -38,5 +40,36 @@ module.exports = {
         .catch(err => {
             res.json(err);
         });  
+    },
+
+    add_quote : function(req, res) {
+        Author.findOneAndUpdate({'_id' : req.params.id}, {$push: {quotes: req.body}}, {runValidators: true})
+            .then(data =>
+                res.json(data)
+            )
+            .catch(err => {
+                res.json(err)
+            })
+    },
+
+    delete_quote : function(req, res) {
+        Author.findOneAndUpdate({'_id' : req.params.id}, {$pull: {'quotes' : req.body}})
+        .then(data =>
+            res.json(data)
+        )
+        .catch(err => {
+            res.json(err)
+        })
+    },
+
+    update_quote : function(req, res) {
+        console.log(req.body);
+        Author.findOneAndUpdate({'_id' : req.params.id, 'quotes._id' : req.body._id}, {$set: {'quotes.$.score' : req.body.score}})
+        .then(data =>
+            res.json(data)
+        )
+        .catch(err => {
+            res.json(err)
+        })
     }
 }
